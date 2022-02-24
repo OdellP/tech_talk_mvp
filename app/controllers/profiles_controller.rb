b491a60567f1fe5dc51f7058e58dff15706eb56e
Odell Purifoy <odell.purifoy@freshly.com>
2022-02-24 18:34:43 -0500

    Fixed ability for user to build a profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/new
   def new
-    @profile = current_user.profile.build
+    @profile = current_user.build_profile
   end
 
   # GET /profiles/1/edit
@@ -22,7 +22,7 @@ class ProfilesController < ApplicationController
 
   # POST /profiles or /profiles.json
   def create
-    @profile = current_user.profile.build(profile_params)
+    @profile = current_user.build_profile(profile_params)
 
     respond_to do |format|
       if @profile.save
